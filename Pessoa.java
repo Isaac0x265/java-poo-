755f00f4a64545f93409851cdcbbb93375ea1c54
isaac <164396358+Isaac0x265@users.noreply.github.com>
2024-11-06 14:15:07 -0300

    parte 1 do projeto

---
+++ Pessoa.java
@@ -0,0 +1,39 @@
+public abstract class Pessoa {
+    private String cpf;
+    private String nome;
+    private int idade;
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public Pessoa(String cpf, String nome, int idade){
+        this.cpf = cpf;
+        this.nome = nome;
+        this.idade = idade;
+
+    }
+}
+
+
